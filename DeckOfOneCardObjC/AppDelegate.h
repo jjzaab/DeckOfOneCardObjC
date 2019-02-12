a790e9d2a75ceb949c480bcc36b1e85f38b8b684
XMS_JZhan <xms_jzhan@XMS-JZhan.local>
2019-02-12 13:35:30 -0700

    Initial Commit

---
+++ DeckOfOneCardObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeckOfOneCardObjC
+//
+//  Created by XMS_JZhan on 2/12/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeckOfOneCardObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckOfOneCardObjC
+//
+//  Created by XMS_JZhan on 2/12/19.
+//  Copyright © 2019 XMS_JZhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
